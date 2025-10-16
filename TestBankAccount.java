ddf400f25c24e85d8db4039b9b8b4b0dbc040382
sergi-e <ishserge16@gmail.com>
2025-10-16 19:56:05 +0000

    Testing Class for the banking system

---
+++ TestBankAccount.java
@@ -0,0 +1,7 @@
+public class TestBankAccount {
+    public static void main(String[] args) {
+        BankAccount obj1=new BankAccount(BankAccount.AccountType.SAVINGS,"Sav101");
+        
+    }
+    
+}
